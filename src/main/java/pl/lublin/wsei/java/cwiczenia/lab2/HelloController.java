597af2ee681e78c45a8526b9fc7327a713abb6ac
Emilia Duda <emilia.duda1995@gmail.com>
2022-04-11 20:01:19 +0200

    dodanie obsługi dodawania pliku i kontrolki ListView

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
@@ -1,14 +1,31 @@
 package pl.lublin.wsei.java.cwiczenia.lab2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Label lbFile;
+    public ListView IstInfografiki;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public  void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if(file !=null){
+            lbFile.setText(file.getAbsolutePath());
+        }
+        else{
+            lbFile.setText("Proszę wczytać plik ...");
+        }
+
     }
 }
